7d858bfa4bebb338f10798891498527fbeb90c60
piee777 <154609112+piee777@users.noreply.github.com>
2025-09-28 17:11:10 +0100

    Update get-ai-response.mjs

--- netlify/functions/get-ai-response.mjs
+++ netlify/functions/get-ai-response.mjs
@@ -2,7 +2,7 @@ export async function handler(event) {
   try {
     const { prompt } = JSON.parse(event.body);
 
-    const apiKey = process.env.VITE_GEMINI_API_KEY;
+    const apiKey = process.env.GEMINI_API_KEY;
 
     const response = await fetch(
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey,
